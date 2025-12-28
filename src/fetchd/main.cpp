e778cf3290a0a9cfe7846a5dc7d2d7b6688017c2
rubnMartnez <projects.overstay918@passinbox.com>
2025-12-28 18:36:58 +0100

    add signal handling

--- src/fetchd/main.cpp
+++ src/fetchd/main.cpp
@@ -1,7 +1,33 @@
 #include "main.h"
 
-int main(){
+#include <atomic>
+#include <csignal>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <cstring>
 
-    std::cout << "fetchd daemon starting...\n";
+// Global flag used by main loop
+static std::atomic<bool> isRunning{true};
 
+// Signal handler
+void handle_signal(int signal) {
+    std::cout << "Received signal: " << signal << '\n';
+    isRunning = false;
+}
+
+int main() {
+    // Register signal handlers
+    std::signal(SIGINT, handle_signal);   // Ctrl+C
+    std::signal(SIGTERM, handle_signal);  // kill
+
+    std::cout << "fetchd: daemon started\n";
+
+    while (isRunning) {
+        std::cout << "fetchd: running\n";
+        std::raise(SIGTERM);
+    }
+
+    std::cout << "fetchd: daemon stopping\n";
+    return 0;
 }
